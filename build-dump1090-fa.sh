5e48877c9e8035aa9365502608580d1cd50801ec
abcd567a <abcd567@hotmail.com>
2023-11-23 16:18:18 -0500

    Update build-dump1090-fa.sh

--- build-dump1090-fa.sh
+++ build-dump1090-fa.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 BUILD_FOLDER=${PWD}
-sudo pacman -Sy --needed git binutils make gcc glibc fakeroot pkgconf autoconf
+sudo pacman -Sy --needed wget git binutils make gcc glibc fakeroot pkgconf autoconf rtl-sdr lighttpd
+sudo wget -O /etc/udev/rules.d/rtl-sdr.rules https://github.com/abcd567a/temp/raw/main/rtl-sdr.rules
 
 cd ${BUILD_FOLDER}/archlinux-dump1090-fa
 makepkg -si
+
+

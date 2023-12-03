c1b2db621a6bf2d57d6ef1a2106986c768b47a56
abcd567a <abcd567@hotmail.com>
2023-12-02 19:48:03 -0500

    Update and rename build-dump1090-fa.sh to dump1090-fa-builder.sh

--- build-dump1090-fa.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BUILD_FOLDER=${PWD}
-sudo pacman -S --needed git fakeroot
-cd ${BUILD_FOLDER}/archlinux-dump1090-fa
-makepkg -si
-
-
---
+++ dump1090-fa-builder.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+DL_DIR=archlinux-dump1090-fa
+INST_DIR=/usr/share/dump1090-fa-builder
+USR=`whoami`
+echo ""
+echo -e "\e[1;32mInstalling Build Tools "fakeroot" (if not already installed)  \e[39m"
+sleep 3
+sudo pacman --needed -S fakeroot
+
+echo ""
+echo -e "\e[1;95mCreating dedicated folder \e[39m" ${INST_DIR} "\e[39m"
+echo -e "\e[1;32mThis folder will hold cloned source-code and built package \e[39m"
+sleep 3
+sudo mkdir ${INST_DIR}
+sudo chown ${USR} ${INST_DIR}
+echo ""
+echo -e "\e[1;32mCloning source-code of dump1090-fa \e[39m"
+echo ""
+sleep 3
+git clone https://github.com/abcd567a/${DL_DIR} ${INST_DIR}/${DL_DIR}
+echo ""
+echo -e "\e[1;95mBuilding package dump1090-fa \e[39m"
+echo ""
+sleep 2
+cd ${INST_DIR}/${DL_DIR}
+makepkg -si
+
+echo ""
+echo ""
+echo -e "\e[1;95mCloned source-code and built package are located in \e[0;39m"
+echo -e "\e[1;95mdedicated folder \e[39m" ${INST_DIR} "\e[0;39m"
+echo -e ""
+
+
+
+
+
